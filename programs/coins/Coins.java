b3e8e49603d9492777da7152aebf5828f3b2cfa3
Hifiboombox <zbradley96@gmail.com>
2013-08-29 09:20:13 -0700

    added programs

---
+++ programs/coins/Coins.java
@@ -0,0 +1,30 @@
+// name:  		Coins.java
+//
+// program: 	CoinsTester
+//
+// purpose:		This class accepts a certain number of monetary change. The output is a list of the number 
+//				of quarters, dimes, nickels, and pennies that will make that amount of change with the least 			
+// 				number of coins possible.  This is a skeleton that will be finished by the students. 
+	 
+public class Coins {
+
+	private int myChange;
+	
+	public Coins(int change) {
+		myChange = change;
+	}
+
+	public void calculate() {
+		
+		String[] coinNames = {"Quarters","Dimes","Nickels","Pennies"};
+		int[] coinValues = {25,10,5,1};
+		int[] coinCount = new int[4];
+		int n = myChange;
+
+		for(int i = 0; i < 4; i++) {
+			coinCount[i] = (n / coinValues[i]);
+			n = n - (coinCount[i] * coinValues[i]);
+			System.out.println(coinNames[i] + ": " + coinCount[i]);
+		}
+	}
+}
---
+++ programs/coins/CoinsTester.java
@@ -0,0 +1,14 @@
+// name:  		CoinsTester.java
+//
+// program:  	CoinsTester
+//
+// purpose: 	Put to use the methods described in Coins.java
+
+public class CoinsTester {
+        
+    public static void main(String[] args) {
+    	
+		Coins money = new Coins(24);
+		money.calculate();
+    }
+}
