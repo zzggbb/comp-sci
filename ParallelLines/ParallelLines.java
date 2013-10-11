4a2664226021c0de411c04fcd7206a1bf2cddb8d
Hifiboombox <zbradley96@gmail.com>
2013-10-10 22:50:10 -0700

    ya

--- ParallelLines/ParallelLines.java
+++ ParallelLines/ParallelLines.java
@@ -2,7 +2,7 @@ import apcslib.*;
 import java.awt.Color;
 
 public class ParallelLines {
-	
+
 	int SQR_SEP = 20;
 	int WIG_OFFSET = 10;
 	int SQR_SIZE = 20;
@@ -10,31 +10,31 @@ public class ParallelLines {
 	int COLS = 7;
 	int WIDTH = 500;
 	int HEIGHT = 500;
-	
+
 	private SketchPad paper;
 	private DrawingTool pen;
-	
+
 	public ParallelLines(){
 		paper = new SketchPad(WIDTH,HEIGHT);
 		pen = new DrawingTool(paper);
 	}
-	
+
 	public void draw(){
-		
+
 		int xOffset = (SQR_SIZE*(COLS-2)+COLS*SQR_SEP)/2;
 		int yOffset = SQR_SIZE*(ROWS-2)/2;
 		int[] offsets = {0,1,0,-1};
 		int index = 0;
-	
+
 		for (int row = 0; row < ROWS; row++){
 			for (int col = 0; col < COLS; col++){
-				
-				int WIG = offsets[index%(offsets.length)];
+
+				int wig = offsets[index%(offsets.length)];
 				int xPos = col*(SQR_SEP+SQR_SIZE)-xOffset;
 				int yPos = row*SQR_SIZE-yOffset;
-				
+
 				pen.up();
-				pen.move(xPos+WIG*WIG_OFFSET,yPos);
+				pen.move(xPos+wig*WIG_OFFSET,yPos);
 				pen.down();
 				pen.setColor(Color.black);
 				pen.fillRect(SQR_SIZE,SQR_SIZE);
@@ -47,5 +47,5 @@ public class ParallelLines {
 			pen.move(xOffset,yAlg);
 		}
 	}
-	
+
 }
