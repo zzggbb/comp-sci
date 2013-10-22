48c069cec63ca48294233f049d3785cd273d1094
hifi <zbradley96@gmail.com>
2013-10-21 17:47:44 -0700

    updated Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,3 +1,9 @@
+/* Name: 	Zane Bradley
+ * File:	Palindrome.java
+ * Purpose: Provide the PalindromeTester.java with a Palindrome object,
+ *			and with it the isPal() method.
+ */
+
 public class Palindrome {
 
     private String input;
@@ -8,9 +14,9 @@ public class Palindrome {
         input = s;
     }
 
-    public int isPal(){
+    public boolean isPal(){
         String strip = input.toLowerCase().replaceAll("[^a-z0-9]", "");
         StringBuffer reverse = new StringBuffer(strip).reverse();
-        return ((strip.length() > 1) && reverse.toString().equals(strip)) ? 1 : 0;
+        return ((strip.length() > 1) && reverse.toString().equals(strip));
     }
 }
--- Palindrome/PalindromeTester.java
+++ Palindrome/PalindromeTester.java
@@ -1,17 +1,24 @@
+/* Name: 	Zane Bradley
+ * File:	PalindromeTester.java
+ * Purpose: Use a Palindrome object and its isPal() method to return 
+ *			whether or not an inputted string is a palindrome.
+ */
+
+
 import java.util.Scanner;
 
 public class PalindromeTester {
 
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        String[] resp = {"IS NOT", "IS"};
         boolean cont = true;
         while (cont){
             System.out.println("Give a string or type q to exit: ");
             String userin = s.nextLine();
-            Palindrome p = new Palindrome(userin);
             cont = !(userin.equals("q"));
-            System.out.println(cont ? "\"" + userin + "\" " + resp[p.isPal()] +" a palindrome" : "Program will exit...");
+            System.out.println(cont ? "\"" + userin + "\" " +
+				(new Palindrome(userin).isPal() ? "IS" : "IS NOT") +
+				" a palindrome" : "Program will exit...");
         }
     }
 }
