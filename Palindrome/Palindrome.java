23911297424b56087ed4bab8ad291301965b274b
hifi <zbradley96@gmail.com>
2013-10-22 17:33:05 -0700

    updated Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,7 +1,6 @@
 /* Name: 	Zane Bradley
  * File:	Palindrome.java
- * Purpose: Provide the PalindromeTester.java with a Palindrome object,
- *			and with it the isPal() method.
+ * Purpose: Provide the PalindromeTester.java with a Palindrome object and the isPal() method.
  */
 
 public class Palindrome {
@@ -15,8 +14,13 @@ public class Palindrome {
     }
 
     public boolean isPal(){
+
+        // convert to lowercase and remove all non-alphanumeric characters
         String strip = input.toLowerCase().replaceAll("[^a-z0-9]", "");
+        // create a string buffer object with 'strip' and reverse it
         StringBuffer reverse = new StringBuffer(strip).reverse();
+        // make sure the stripped version has more than 1 significant character and
+        // is the same word as its reversed version
         return ((strip.length() > 1) && reverse.toString().equals(strip));
     }
 }
--- Palindrome/PalindromeTester.java
+++ Palindrome/PalindromeTester.java
@@ -1,23 +1,23 @@
 /* Name: 	Zane Bradley
  * File:	PalindromeTester.java
- * Purpose: Use a Palindrome object and its isPal() method to return
- *			whether or not an inputted string is a palindrome.
+ * Purpose: Use a Palindrome object and its isPal() method to return whether or not an inputted string is a palindrome.
  */
-
 
 import java.util.Scanner;
 
 public class PalindromeTester {
 
     public static void main(String[] args){
+
         Scanner s = new Scanner(System.in);
         boolean cont = true;
         while (cont){
-            System.out.println("Give a string or type q to exit: ");
+            System.out.print("Give a string or type q to exit: ");
             String userin = s.nextLine();
             cont = !(userin.equals("q"));
-            boolean check = new Palindrome(userin).isPal();
-            System.out.println(cont ? "\"" + userin + "\" " + (check ? "IS":"IS NOT") + " a palindrome":"Program will exit...");
+            boolean checkPal = new Palindrome(userin).isPal();
+            // print whether or not userin is a palindrome, or exit depending on supplied string
+            System.out.print(cont ? "\""+userin+"\" "+(checkPal ? "IS":"IS NOT")+" a palindrome\n":"Program will exit...");
         }
     }
 }
