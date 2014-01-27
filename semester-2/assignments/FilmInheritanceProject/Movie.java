63fc5788f6b6dc8df62c3294b62b658c61828764
Hifiboombox <zbradley96@gmail.com>
2014-01-27 15:45:48 -0800

    added FilmInheritanceProject

---
+++ semester-2/assignments/FilmInheritanceProject/Movie.java
@@ -0,0 +1,6 @@
+public class Movie extends Film {
+
+    public Movie(String name, String producer, String director, int year,
+                String[] actors, double length, )
+        super(name, producer, director, year, actors, )
+}
---
+++ semester-2/assignments/FilmInheritanceProject/Television.java
@@ -0,0 +1,3 @@
+public class Television extends Film {
+    private int episodes
+}
