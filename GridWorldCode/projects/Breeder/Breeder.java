2c1851989e2995246e300bc2c34d6b6c3f38b38f
Zane Bradley <zbradley96@gmail.com>
2014-03-20 23:07:01 -0700

    working Breeder??

--- GridWorldCode/projects/Breeder/Breeder.java
+++ GridWorldCode/projects/Breeder/Breeder.java
@@ -1,127 +1,153 @@
-import info.gridworld.grid.Grid;
-import info.gridworld.grid.Location;
+import info.gridworld.grid.*;
 import info.gridworld.actor.Actor;
 import java.awt.Color;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Breeder extends Actor {
-  
-	private Grid<Actor> grid;
-  	//private Location location;
-  	private int direction;
-  	private int age;
-  	private int deathChance;
-  	private Random rand;
- 
-   	private final int CHILD = 6;
-	private final int MIDDLE = 20;
-	private final int ELDER = 30;
 
-	public Breeder() {
-    	//super();
-		grid = getGrid();
-		direction = 0;
-        setColor(Color.BLUE);
-		age = 0;
-        deathChance = 0;
-        rand = new Random();
-  	}
-	public Location getInFront(){
-		return getLocation().getAdjacentLocation(getDirection());
-	}
+  private int age;
+  private int deathChance;
+  private Random rand;
+  private boolean isAlive;
+  private final int CHILD = 5;
+  private final int MIDDLE = 15;
 
-  	public void act() {
-    	maybeDie();
-    	if (!canMove()){
-			Object thingInFront = grid.get(getInFront());
-			if (thingInFront instanceof Breeder){
-				Breeder breederInFront = (Breeder) thingInFront;
-				if (getAge() >= ELDER){
-					int potentialFoodAge = breederInFront.getAge();
-					if (potentialFoodAge <= CHILD){
-						setAge(getAge()-potentialFoodAge);					
-						eat();
-						// ATE A BREEDER
-					} else {
-						// breeder in front of us is too old
-					}
-				} else {
-					// we are not old enough to eat a child
-				}
-			} else {
-				//there is something in front of us, it is not a breeder
-				setDirection(getDirection()-45);
-			}
-		} else {
-			// nothing is in front of us
-			move();	
-		}
-    	ageColor();
-    	age++;
-    	deathChance += 10;
-  	}
-  	public void eat(){
-		Breeder victim = (Breeder) grid.get(getInFront());
-		int age = victim.getAge();
-		Location loc = victim.getLocation();
-		System.out.println("Breeder at " + loc + " was eaten at age " + age);
-		victim.removeSelfFromGrid();
-	}
-	public int getAge(){
-    	return age;
-  	}
-	public void setAge(int n){
-		age = n;
-	}
+  public Breeder() {
 
-  	public void ageColor() {
-		if (age > CHILD){
-			if (age > MIDDLE){
-				if (age > ELDER){
-					setColor(Color.YELLOW);
-					return;
-				}
-				setColor(Color.GREEN);
-				return;
-			}
-			setColor(Color.RED);
-			return;
-		}
-  	}
-  	public void maybeDie() {
-    	if (rand.nextInt()*100 < deathChance) {
-      		removeSelfFromGrid();
-		}
+    setColor(Color.BLUE);
+    age = 0;
+    deathChance = 0;
+    rand = new Random();
+    isAlive = true;
+  }
+  public Location getInFront(){
+    return getLocation().getAdjacentLocation(getDirection());
+  }
+
+ public void act() {
+    maybeDie();
+    if (isAlive) {
+      if (!canMove()) {
+        if (getGrid().isValid(getInFront())) {
+          Object thingInFront = getGrid().get(getInFront());
+          if (thingInFront instanceof Breeder){
+            Breeder breederInFront = (Breeder) thingInFront;
+            if (getAge() > MIDDLE){
+              int potentialFoodAge = breederInFront.getAge();
+              if (potentialFoodAge <= CHILD){
+                setAge(getAge()-potentialFoodAge);     
+                eat();
+                // ATE A BREEDER
+              }
+            }
+          } else {
+            //there is something in front of us, it is not a breeder          
+            turn();
+          }
+        } else { 
+          turn();
+        } 
+      } else {
+        // nothing is in front of us
+        move(); 
+      }
+      breed();
+      ageColor();
+      age++;
+      if (age <= 3) {
+        deathChance = 3;
+      } else if (age <= 15) {
+        deathChance = 7;
+      } else {
+        deathChance = 10;
+      }
     }
-  	public int getDirection() {
-    	return direction;
-  	}
-  	public void setDirection(int val) {
-    	direction = val;
-  	}
-  	public boolean canMove() {
-		Grid<Actor> gr = getGrid();
-        if (gr == null)
-			System.out.println("grid is null");
-            return false;
-        Location loc = getLocation();
-        Location next = loc.getAdjacentLocation(getDirection());
-        if (!gr.isValid(next))
-			System.out.println("next space isn't valid");
-            return false;
-        Actor neighbor = gr.get(next);
-        return (neighbor == null);
-  	}
-  	public void move() {
-  		if (grid == null){
-			System.out.println("grid is null"); 
-			return;
-		}
-		Location next = getInFront();
-		if (grid.isValid(next)){
-			moveTo(next);
-		} else {
-			removeSelfFromGrid();
-		}
-  	}
+  }
+   
+   
+  public void turn(){
+    setDirection(getDirection()-45);
+  }
+
+  public void eat(){
+    Breeder victim = (Breeder) getGrid().get(getInFront());
+    int age = victim.getAge();
+    Location loc = victim.getLocation();
+    System.out.println("Breeder at " + loc + " was eaten at age " + age);
+    victim.removeSelfFromGrid();
+  }
+  public int getAge(){
+    return age;
+  }
+ 
+  public void breed() {
+    if (getGrid().isValid(getInFront())) {
+      if (getGrid().get(getInFront()) instanceof Breeder) {
+        Breeder thingInFront = (Breeder)getGrid().get(getInFront());
+        if (age > 5 && age <= 15 && (thingInFront.getAge()) > 5 && (thingInFront.getAge()) <= 15) {
+          Breeder breederInFront = (Breeder) thingInFront;
+          ArrayList childSpaces = getGrid().getValidAdjacentLocations(getLocation());
+          for (int i = 0; i<childSpaces.size(); i++) {
+            Actor child = new Breeder();
+            child.putSelfInGrid(getGrid(), (Location) childSpaces.get(i));
+            child.setDirection(getDirection());
+          }
+        }
+      }
+    }
+  }
+ 
+
+  public void setAge(int n){
+    age = n;
+  }
+
+  public void ageColor() {
+    if (age > CHILD){
+      if (age > MIDDLE){
+        setColor(Color.CYAN);
+        return;
+      }
+      setColor(Color.RED);
+      return;
+    }
+  }
+
+  public void maybeDie() {
+    if (rand.nextInt(100) < deathChance) {
+      System.out.println("Breeder at "+ getLocation() + " died at age " + age); 
+      removeSelfFromGrid();
+      isAlive = false;  
+    }
+  }
+  
+  public boolean canMove() {
+    Grid<Actor> gr = getGrid();
+    if (gr == null){
+      //System.out.println("grid is null");
+      return false;
+    }
+    Location loc = getLocation();
+    Location next = loc.getAdjacentLocation(getDirection());
+    if (!gr.isValid(next)){
+      //System.out.println("next space isn't valid");
+      return false;
+    }
+    Actor neighbor = gr.get(next);
+    return (neighbor == null);
+  }
+
+  public void move() {
+    if (getGrid() == null){
+      //System.out.println("grid is null"); 
+      return;
+    }
+    Location next = getInFront();
+    if (getGrid().isValid(next)){
+      moveTo(next);
+    } else {
+      removeSelfFromGrid();
+    }
+  }
 } 
--- GridWorldCode/projects/Breeder/BreederRunner.java
+++ GridWorldCode/projects/Breeder/BreederRunner.java
@@ -1,26 +1,19 @@
 import info.gridworld.actor.*;
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Rock;
 import info.gridworld.grid.Location;
 
 public class BreederRunner {
 
-	final static int BREEDER_COUNT = 10;
-	final static int ROCK_COUNT = 3;
+ public static void main(String[] args){
+  ActorWorld world = new ActorWorld();
 
-	public static void main(String[] args){
-		ActorWorld world = new ActorWorld();
-/*
-		for (int i=0; i<BREEDER_COUNT; i++){
-			world.add(new Breeder());
-		}
-		for (int i=0; i<ROCK_COUNT; i++){
-			world.add(new Rock());
-		}
-*/
-		Breeder x = new Breeder();
-		world.add(new Rock());
-		world.add(x);
-		world.add(new Bug());
-		world.show();	
-	}
+  for (int i = 0; i<10; i++) {
+  Breeder x = new Breeder();
+  world.add(x);
+  }
+
+  
+  world.show(); 
+ }
 }
-
