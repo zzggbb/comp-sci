108941a72af54c495e5b927f0d514f9e5cc0258d
Zane Bradley <zbradley96@gmail.com>
2014-03-27 11:39:11 +0000

    critters

---
+++ GridWorldCode/projects/Breeder/intTest.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class intTest {
+    public static void main(String[] args){
+        Random r = new Random();
+        while(true){
+            System.out.println(r.nextInt());
+        }
+    }
+} 
---
+++ GridWorldCode/projects/critters/ChameleonKid.java
@@ -0,0 +1,9 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import java.util.ArrayList;
+
+public class ChameleonKid extends ChameleonCritter {
+    
+
+}
