015d945218bc3317a4cfc8f98417c2dd1b924266
Zane Bradley <zbradley96@gmail.com>
2014-04-01 10:55:27 +0000

    backup before headers

--- GridWorldCode/projects/critters/BlusterCritter.java
+++ GridWorldCode/projects/critters/BlusterCritter.java
@@ -1,3 +1,12 @@
+/*
+ * Name:  Zane Bradley
+ * File:  BlusterCritter.java
+ * Desc:  A BlusterCritter is instantiated with a `c` value which
+ *        represents its courage. A BlusterCritter checks the 
+ *        number of actors within 2 spaces, and will "blush" if the 
+ *        there are less actors than the "courage" value.
+ */
+
 import java.util.ArrayList;
 import java.awt.Color;
 import info.gridworld.actor.Actor;
@@ -6,51 +15,48 @@ import info.gridworld.grid.Location;
 
 public class BlusterCritter extends Critter {
 	
-	private int c;
+    private int c;
 
-	public BlusterCritter(int courage){
-		super();
-		c = courage;
-	}
+    public BlusterCritter(int courage){
+        super();
+	   c = courage;
+    }
+	
+    public ArrayList<Actor> getActors(){
+        ArrayList<Actor> neighbors = new ArrayList<Actor>();
+	   Location center = getLocation();
+	   int cX = center.getRow();
+	   int cY = center.getCol();
+	   for (int x=cX-2 ; x<cX+3 ; x++){
+	       for (int y=cY-2 ; y<cY+3 ; y++){
+		      Location cLoc = new Location(x,y);
+			 if(getGrid().isValid(cLoc) && !(cLoc.equals(center))){
+			     neighbors.add(getGrid().get(cLoc));
+			 }
+		  } 
+	   }
+	   return neighbors;
+    }
+    public void darken(double deltaRGB){
+	   Color c = getColor();
+	   int[] colors = {
+            (int)(c.getRed()+deltaRGB),
+            (int)(c.getGreen()+deltaRGB),
+            (int)(c.getBlue()+deltaRGB)
+        }
+	   for (int i=0; i<colors.length(); i++){
+            if (colors[i] < 0){
+                colors[i] = 0;
+            } else if(colors[i] > 255 {
+                colors[i] = 255;
+            }
+        }
+	   setColor(new Color(colors[0],colors[1],color[2]));
+	   System.out.println("Changed color");	
+    }
 
-	/*
-	 *	. . . . . 
-	 *	. . . . . 
-	 *	. . $ . .  
-	 *  . . . . .  
-	 *  . . . . . 
-	 */
-	public ArrayList<Actor> getActors(){
-		ArrayList<Actor> neighbors = new ArrayList<Actor>();
-		Location center = getLocation();
-		int cX = center.getRow();
-		int cY = center.getCol();
-		for (int x=cX-2 ; x<cX+3 ; x++){
-			for (int y=cY-2 ; y<cY+3 ; y++){
-				Location cLoc = new Location(x,y);
-				if(getGrid().isValid(cLoc) && !(cLoc.equals(center))){
-					neighbors.add(getGrid().get(cLoc));
-				}
-			} 
-		}
-		return neighbors;
-	}
-	public void darken(double deltaRGB){
-		Color c = getColor();
-		
-		int red = (int)(c.getRed()*(1-deltaRGB));
-		int grn = (int)(c.getGreen()*(1-deltaRGB));
-		int blu = (int)(c.getBlue()*(1-deltaRGB));
-		
-		int redAdj = red<255?red:255;
-		int grnAdj = grn<255?grn:255;
-		int bluAdj = blu<255?blu:255;
-
-		setColor(new Color(redAdj,grnAdj,bluAdj));
-		System.out.println("Changed color");	
-	}
-
-	public void processActors(ArrayList<Actor> actors){
-		darken(((actors.size()<c)?-1:1)*0.05);
-	}
+    public void processActors(ArrayList<Actor> actors){
+        int deltaRGB = ((actors.size()<c)?1:-1)*10;	
+        darken(deltaRGB);
+    }
 }
